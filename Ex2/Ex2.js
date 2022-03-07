b326e95b6dbaf1408eb2980ccd7206d5caaf9507
Nuno Miguel <nm.albano20@gmail.com>
2022-03-07 15:44:39 +0000

    fix no relogio automatico

--- Ex2/Ex2.js
+++ Ex2/Ex2.js
@@ -1,10 +1,12 @@
 $(document).ready(function (){
-    function horas(){
+    function relogio(){
         var d=new Date();
         var h=d.getHours();
         var m=d.getMinutes();
         var s=d.getSeconds();
         var d=h+":"+m+":"+s;
+        $("#escrever").html(d);
+        setTimeout(relogio,1000);
     }
-    horas();
+    relogio();
 });
