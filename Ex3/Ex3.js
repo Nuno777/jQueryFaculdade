6283b7a9a2a5503f54304f1b266a67f0b12c4046
Nuno Miguel <nm.albano20@gmail.com>
2022-03-07 16:24:44 +0000

    fix de uma imagem com over and down

--- Ex3/Ex3.js
+++ Ex3/Ex3.js
@@ -1,3 +1,13 @@
 $(document).ready(function (){
-   
+    $("#imagem").hover(function(){
+        $(this).attr('src','img/alunos_over.gif')
+    },function(){
+        $(this).attr('src','img/alunos_down.gif')
+    });
+    $("#imagem").mousedown(function(){
+        $(this).attr('src','img/alunos_down.gif')
+    });
+    $("#imagem").mouseout(function(){
+        $(this).attr('src','img/alunos.gif')
+    });
 });
