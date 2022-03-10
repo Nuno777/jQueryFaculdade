f1b02c5ee563cbef00b1f1803868125ab847b0cd
Nuno Miguel <nm.albano20@gmail.com>
2022-03-10 11:53:03 +0000

    forms jquery

--- Ex5/Ex5.js
+++ Ex5/Ex5.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-    
+    $("#formdados").submit(function () {
+        return validar();
+    });
+    function validar() {
+        var mensagem = "\nErros na submissão:";
+        var nome = $("#name").val().trim();
+        var email = $("#email").val().trim();
+        var idade = $("#age").val().trim();
+        var telefone = $("#tel").val().trim();
+        var password = $("#password").val().trim();
+        var passwordConfirm = $("#passwordConfirm").val().trim();
+
+        if (nome == "" || nome.length == 0) {
+            alert(mensagem+"Erro no nome");
+        }else if(password != passwordConfirm){
+            alert(mensagem+"Password errada")
+        }
+
+    }
 });
