a65ef13b01cc9ea3cbbde7bb772c91567a0c1523
Nuno Miguel <nm.albano20@gmail.com>
2022-03-14 16:38:24 +0000

    fix na idade e no email corrigidos

--- Ex6/Ex6.js
+++ Ex6/Ex6.js
@@ -2,52 +2,63 @@ $(document).ready(function () {
     $("#formdados").submit(function () {
         return validar();
     });
-    function validar() {
-        var mensagem = "\nErros na submissão:";
+
+    $("span").hide();
+    $("#name").on("change keyup", verificarNome);
+    $("#email").on("change keyup", verificarEmail);
+    $("#age").on("change keyup", verificarIdade);
+    $("#tel").on("change", verificarTelefone);
+
+    /*  function validar() {
+         $(".error").hide();
+ 
+         verificarNome();
+         verificarEmail();
+         verificarIdade();
+         verificarTelefone();
+ 
+         if ($(".error:visible").length > 0) {
+             return false;
+         }
+         return true;
+     } */
+
+    function verificarNome() {
         var nome = $("#name").val().trim();
-        var email = $("#email").val().trim();
-        var idade = $("#age").val().trim();
-        var telefone = $("#tel").val().trim();
-        var pass = $("#password").val().trim();
-        var passConfirm = $("#passwordConfirm").val().trim();
-        var testEmail = /^\S+@\S+.\S+$/;
-
-        if (!testEmail.test(email)) {
-            mensagem += "\nCampo email deve ter um email valido";
+        if (nome.length === 0) {
+            $(".errorNome").show();
+        } else {
+            $(".errorNome").hide();
         }
-        if (nome === "") {
-            mensagem += "\nCampo nome deve estar preenchido";
-        }
-        if (email.length === 0) {
-            mensagem += "\nCampo email deve estar preenchido";
-        }
-        if (telefone.length === 0) {
-            mensagem += "\nCampo telefone deve estar preenchido";
-        }
-        if (idade.length === 0) {
-            mensagem += "\nCampo idade deve eatar preenchido";
-        }
-
-        if (isNaN(telefone) || Number.isInteger(Number(telefone)) === false) {
-            mensagem += "\nCampo telefone deve ser um numero com 9 digitos";
-        }
-        if (isNaN(idade) || Number.isInteger(Number(idade)) === false) {
-            mensagem += "\nCampo idade deve ser um numero superior ou igual a 18";
-        }
-
-        //password
-        if (pass.length == 0) {
-            mensagem += "\nCampo password deve esatr preenchido";
-        } else if (pass.length < 6) {
-            mensagem += "\nCampo password deve ter pelo menos 6 digitos";
-        } else if (pass != passConfirm) {
-            mensagem += "\nPassword diferentes";
-        }
-
-        if (mensagem !== "Erros na submissao:") {
-            alert(mensagem);
-            return false;
-        }
-        return true;
     }
+
+    function verificarEmail() {
+        var email = $("#email").val().trim();
+        var testEmail = /^\S+@\S+.\S+$/;
+        if (email.length === 0 || !testEmail(email)) {
+            $(".errorEmail").hide();
+        } else {
+            $(".errorEmail").show();
+        }
+    }
+
+    function verificarIdade() {
+        var idade = $("#age").val().trim();
+        if (idade < 18) {
+            $(".errorIdade").show();
+        } else {
+            $(".errorIdade").hide();
+        }
+    }
+
+    function verificarTelefone() {
+        var telefone = $("#tel").val().trim();
+        if (telefone < 9) {
+            $(".errorTelefone").show();
+        } else {
+            $(".errorTelefone").hide();
+        }
+
+    }
+
 });
